f9edf5ca98043351a841cb8ceb9dd9d6f17b6f5a
vaibhav shamrao pawar <pawarvaibhav1103@gmail.com>
2021-10-03 10:33:33 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply{
+    
+    public static void main(String [] args){
+        int a=3;
+        int b=7;
+        int add=a*b;
+        System.out.println("addition is : " + add);
+    }
+}
